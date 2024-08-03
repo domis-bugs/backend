24f3aa4b8549d3dc3d93e16ea621b4ec5b893ac5
Yuliet Murcia <am514868@gmail.com>
2024-08-03 16:42:56 -0500

    commit inicial

---
+++ src/main/java/com/domicilios/bugsbunny_api/BugsbunnyApiApplication.java
@@ -0,0 +1,13 @@
+package com.domicilios.bugsbunny_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugsbunnyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugsbunnyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domicilios/bugsbunny_api/BugsbunnyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domicilios.bugsbunny_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugsbunnyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
